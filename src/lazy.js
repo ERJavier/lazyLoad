532a11bab9fe7ce03ea4c2598ba591da0682a04d
Javier Cepeda <erjavierc@gmail.com>
2022-08-20 14:11:01 -0400

    lazyLoad

--- src/lazy.js
+++ src/lazy.js
@@ -1,16 +1,16 @@
 const isIntersecting = (entry) => {
-
     return entry.isIntersecting 
-}
+};
 
-const accion = () => {
-    
-}
+const accion = (entry) => {
+    const nodo = entry.target
+    observer.unobserve(nodo)
+};
 const observer = new IntersectionObserver((entries) => {
     entries
         .filter(isIntersecting)
         .forEach(accion)
-})
+});
 
  export const registerImage = (imagen) => {
     observer.observe(imagen);
